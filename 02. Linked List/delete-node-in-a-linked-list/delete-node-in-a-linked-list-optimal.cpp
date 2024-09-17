37934be7f507c3c0d79c36c6222c4db26eb69d11
Muaz Hussain <muaz.self@gmail.com>
2024-09-17 16:28:51 +0600

    Delete node in a link list done

---
+++ 02. Linked List/delete-node-in-a-linked-list/delete-node-in-a-linked-list-optimal.cpp
@@ -0,0 +1,19 @@
+// Problem link: https://leetcode.com/problems/delete-node-in-a-linked-list/
+// Time: O(1)
+// Space: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
